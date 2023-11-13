c7d9015a532ef731c3a37fa961382189ebc0406d
Evgeny Kapusta <kpae@viessmann.com>
2023-11-13 13:17:27 +0100

    feat: add easymotion plugin

--- lua/theprimeagen/packer.lua
+++ lua/theprimeagen/packer.lua
@@ -45,7 +45,7 @@ return require('packer').startup(function(use)
   use("mbbill/undotree")
   use("tpope/vim-fugitive")
   use("nvim-treesitter/nvim-treesitter-context");
-
+  use("easymotion/vim-easymotion");
   use {
 	  'VonHeikemen/lsp-zero.nvim',
 	  branch = 'v1.x',
