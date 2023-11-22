5d4bfcf6565f72c40fb882c8d967210ee03d7383
Evgeny K <3193631@gmail.com>
2023-11-22 17:00:15 +0100

    feat: setup for lua, copilot

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "bash", "vimdoc", "javascript", "typescript", "lua", "rust", "go" },
+  ensure_installed = { "bash", "vimdoc", "javascript", "typescript", "lua", "rust", "go","perl" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
